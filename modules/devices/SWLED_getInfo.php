d422761a1bcde15dd25b279de5b268ea58e44bf0
Паша <odinwolk@gmail.com>
2022-03-13 08:14:33 +0300

    Delete SWLED_getInfo.php

--- modules/devices/SWLED_getInfo.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//get information
-$url = 'http://'. $this->getProperty("ip")."/json";
-$content = getUrl($url);
-$json = json_decode($content, true);
-$this->setProperty("effects", json_encode($json["effects"]));
-$this->setProperty("palettes", json_encode($json["palettes"]));
-$this->setProperty("version", $json["info"]["ver"]);
