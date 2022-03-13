8543652a331c008206d7f9f89f0c7cfa96f3c5c0
Паша <odinwolk@gmail.com>
2022-03-13 08:09:50 +0300

    Update and rename SWLED_colorUpdated.php to SLedLamp_colorUpdated.php

---
+++ modules/devices/SLedLamp_colorUpdated.php
@@ -0,0 +1,13 @@
+<?php
+
+if ($params['NEW_VALUE'] == $params['OLD_VALUE']) return;
+
+if (strpos($params['SOURCE'], "CURR") !== FALSE ) return;
+if (strpos($params['SOURCE'], "TMR") !== FALSE ) return;
+if (strpos($params['SOURCE'], "ALMS") !== FALSE ) return;
+
+
+$hsv = hexToHsv($params['NEW_VALUE']);
+$this->setProperty('effect', 16); // set effect Color 
+$this->setProperty('brightness', intval($hsv[2]*255)); 
+$this->setProperty('scale', intval($hsv[0]/360*255));
